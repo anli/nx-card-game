f092e83f3a6dba7cd4576d47f35aa61122e57546
Su Anli <su.anli@anlitz.com>
2022-03-13 11:30:30 +0800

    chore(nx): add github actions test, lint

--- apps/card-game-app/src/app/app.spec.tsx
+++ apps/card-game-app/src/app/app.spec.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
 import { render } from '@testing-library/react-native'
+import React from 'react'
+import { App } from './app'
 
-import App from './app'
-
-test('renders correctly', () => {
-  const { getByText } = render(<App />)
-  expect(getByText('Welcome')).toBeDefined()
+describe('Given I am at Game Screen', () => {
+  it('Then I should see Restart Button, And I should see Steps Count, And I should see Cards', () => {
+    const { getByText } = render(<App />)
+    expect(getByText('Welcome')).toBeDefined()
+  })
 })
