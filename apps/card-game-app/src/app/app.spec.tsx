cf7db223cfa8c7bbb54bcf6264cd137620e262f7
Su Anli <su.anli@anlitz.com>
2022-03-13 13:58:30 +0800

    feat(app): add GameScreen UI

--- apps/card-game-app/src/app/app.spec.tsx
+++ apps/card-game-app/src/app/app.spec.tsx
@@ -1,10 +1,36 @@
-import { render } from '@testing-library/react-native'
+import '@testing-library/jest-native/extend-expect'
+import { fireEvent, render } from '@testing-library/react-native'
 import React from 'react'
 import { App } from './app'
 
 describe('Given I am at Game Screen', () => {
   it('Then I should see Restart Button, And I should see Steps Count, And I should see Cards', () => {
-    const { getByText } = render(<App />)
-    expect(getByText('Welcome')).toBeDefined()
+    const { getByText, getByTestId, getAllByTestId } = render(<App />)
+
+    expect(getByText('Restart')).toBeDefined()
+
+    expect(getByTestId('StepCounter')).toHaveTextContent('STEPS:0')
+
+    expect(getAllByTestId('Card')).toHaveLength(12)
+  })
+
+  it('When device screen size changes, Then I should see Cards resized', async () => {
+    const { getByTestId, getAllByTestId } = render(<App />)
+    const cardsHeight = 400
+    const cardsWidth = 400
+
+    fireEvent(getByTestId('Cards'), 'layout', {
+      nativeEvent: {
+        layout: {
+          width: cardsWidth,
+          height: cardsHeight
+        }
+      }
+    })
+
+    expect(getAllByTestId('Card')[0]).toHaveStyle({
+      width: cardsWidth / 3,
+      height: cardsHeight / 4
+    })
   })
 })
--- apps/card-game-app/src/app/app.tsx
+++ apps/card-game-app/src/app/app.tsx
@@ -1,11 +1,65 @@
 import { Screen, Text, ThemeProvider, View } from '@nx-card-game/shared/ui'
-import React from 'react'
+import React, { useState } from 'react'
+import { Pressable } from 'react-native'
+
+const cards = Array.from({ length: 12 }, (_, k) => {
+  return { id: k }
+})
 
 const GameScreen = (): React.ReactElement => {
+  const [cardHeight, setCardHeight] = useState(0)
+  const [cardWidth, setCardWidth] = useState(0)
+
   return (
-    <Screen>
-      <View>
-        <Text>Welcome</Text>
+    <Screen backgroundColor="surface" flex={1}>
+      <View flexDirection="row" justifyContent="space-between">
+        <Pressable>
+          <View padding="base">
+            <Text color="primary" fontSize={20} fontWeight="bold">
+              Restart
+            </Text>
+          </View>
+        </Pressable>
+
+        <View flexDirection="row" alignItems="flex-end" testID="StepCounter">
+          <Text color="text" fontSize={30} marginBottom="extraTight">
+            STEPS:
+          </Text>
+          <Text color="primary" fontSize={48}>
+            0
+          </Text>
+        </View>
+      </View>
+      <View
+        testID="Cards"
+        flex={1}
+        flexDirection="row"
+        flexWrap="wrap"
+        onLayout={(ev) => {
+          setCardHeight(ev.nativeEvent.layout.height)
+          setCardWidth(ev.nativeEvent.layout.width)
+        }}>
+        {cards.map(({ id }) => (
+          <View
+            testID="Card"
+            key={id}
+            width={cardWidth / 3}
+            height={cardHeight / 4}
+            padding="extraTight">
+            <View
+              flex={1}
+              backgroundColor="primary"
+              borderColor="border"
+              borderWidth={4}
+              borderRadius="baseTight"
+              alignItems="center"
+              justifyContent="center">
+              <Text color="text" fontSize={40}>
+                ?
+              </Text>
+            </View>
+          </View>
+        ))}
       </View>
     </Screen>
   )
