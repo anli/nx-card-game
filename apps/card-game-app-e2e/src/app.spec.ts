595a2c9f6bc844924a88d95a323d0dc3a9b1c582
Su Anli <su.anli@anlitz.com>
2022-03-15 00:59:49 +0800

    feat(app): add detox

--- apps/card-game-app-e2e/src/app.spec.ts
+++ apps/card-game-app-e2e/src/app.spec.ts
@@ -1,13 +1,14 @@
-import { device, element, by, expect } from 'detox'
+import { by, device, element, expect } from 'detox'
 
-describe('CardGameApp', () => {
+describe('Given I open App ', () => {
   beforeEach(async () => {
     await device.reloadReactNative()
   })
 
-  it('should display welcome message', async () => {
-    await expect(element(by.id('heading'))).toHaveText(
-      'Welcome CardGameApp 👋'
-    )
+  it('Then I should see Restart Button, And I should see steps counts, And I should see Cards', async () => {
+    await expect(element(by.text('Restart'))).toBeVisible()
+    await expect(element(by.text('STEPS:'))).toBeVisible()
+    await expect(element(by.id('StepCounter'))).toHaveText('0')
+    await expect(element(by.text('?')).atIndex(11)).toBeVisible()
   })
 })
