0faf44f91574c3319c9cd663581f129aeb261c47
Su Anli <su.anli@anlitz.com>
2022-03-14 20:52:56 +0800

    feat(app): add memo GameHeader, FlipCard

--- apps/card-game-app/src/screens/game-screen/game-screen.tsx
+++ apps/card-game-app/src/screens/game-screen/game-screen.tsx
@@ -1,7 +1,8 @@
-import { Screen, Text, View } from '@nx-card-game/shared/ui'
-import React, { useState } from 'react'
-import { Alert, Pressable } from 'react-native'
+import { Screen, View } from '@nx-card-game/shared/ui'
+import React, { useCallback, useState } from 'react'
+import { Alert } from 'react-native'
 import { ANIMATION_DURATION_MILLISECONDS, FlipCard } from './flip-card'
+import { GameHeader } from './game-header'
 import { getRandomNumberPairs } from './get-random-number-pairs'
 
 const INCORRECT_MATCH_DELAY_MILLISECONDS = 1000
@@ -31,63 +32,54 @@ export const GameScreen = ({
   const [flipCardIds, setFlipCardIds] = useState<number[]>(defaultFlipCardIds)
   const [steps, setSteps] = useState<number[]>([])
 
-  const handleRestart = (): void => {
+  const handleRestart = useCallback((): void => {
     setFlipCardIds([])
     setSteps([])
 
     setTimeout(() => {
       setCards(getCards())
     }, ANIMATION_DURATION_MILLISECONDS)
-  }
+  }, [])
 
-  const handleBackCardPress = (id: number): void => {
-    setSteps([...steps, id])
-    setFlipCardIds([...flipCardIds, id])
+  const handleBackCardPress = useCallback(
+    (id: number): void => {
+      setSteps([...steps, id])
+      setFlipCardIds([...flipCardIds, id])
 
-    if (flipCardIds.length > 0 && flipCardIds.length % 2 !== 0) {
-      const [previousCardId] = flipCardIds.slice(-1)
-      const { value: previousCardValue } = cards[previousCardId]
-      const { value: currentCardValue } = cards[id]
+      if (flipCardIds.length > 0 && flipCardIds.length % 2 !== 0) {
+        const [previousCardId] = flipCardIds.slice(-1)
+        const { value: previousCardValue } = cards[previousCardId]
+        const { value: currentCardValue } = cards[id]
 
-      if (previousCardValue !== currentCardValue) {
-        setTimeout(() => {
-          setFlipCardIds(flipCardIds.slice(0, -1))
-        }, INCORRECT_MATCH_DELAY_MILLISECONDS)
-        return
+        if (previousCardValue !== currentCardValue) {
+          setTimeout(() => {
+            setFlipCardIds(flipCardIds.slice(0, -1))
+          }, INCORRECT_MATCH_DELAY_MILLISECONDS)
+          return
+        }
+
+        if (flipCardIds.length + 1 === cards.length) {
+          setTimeout(() => {
+            Alert.alert(
+              'Congratulations',
+              `You win this game by ${steps.length + 1} steps!`,
+              [{ text: 'Try another round', onPress: handleRestart }]
+            )
+          }, ANIMATION_DURATION_MILLISECONDS)
+        }
       }
-
-      if (flipCardIds.length + 1 === cards.length) {
-        setTimeout(() => {
-          Alert.alert(
-            'Congratulations',
-            `You win this game by ${steps.length + 1} steps!`,
-            [{ text: 'Try another round', onPress: handleRestart }]
-          )
-        }, ANIMATION_DURATION_MILLISECONDS)
-      }
-    }
-  }
+    },
+    [cards, flipCardIds, steps, handleRestart]
+  )
 
   return (
     <Screen backgroundColor="surface" flex={1} testID="GameScreen">
-      <View flexDirection="row" justifyContent="space-between">
-        <Pressable onPress={handleRestart}>
-          <View padding="base">
-            <Text color="primary" fontSize={20} fontWeight="bold">
-              Restart
-            </Text>
-          </View>
-        </Pressable>
-
-        <View flexDirection="row" alignItems="flex-end">
-          <Text color="text" fontSize={30} marginBottom="extraTight">
-            STEPS:
-          </Text>
-          <Text color="primary" fontSize={48} testID="StepCounter">
-            {steps.length}
-          </Text>
-        </View>
-      </View>
+      <GameHeader
+        onRestartPress={handleRestart}
+        buttonTitle="Restart"
+        stepLabel="STEPS:"
+        stepCount={steps.length}
+      />
       <View
         testID="Cards"
         flex={1}
